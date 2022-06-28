1a4aaaec16809a8ba8776efde2a1d10ff9bab76c
David Paucar <soydavidpaucar@gmail.com>
2022-06-27 22:57:45 -0500

    :technologist: imported fechas and saldos controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { FechasController } from './fechas/fechas.controller';
+import { FechasModule } from './fechas/fechas.module';
+import { FechasService } from './fechas/fechas.service';
+import { SaldosService } from './saldos/saldos.service';
+import { SaldosController } from './saldos/saldos.controller';
+import { SaldosModule } from './saldos/saldos.module';
 
 @Module({
-  imports: [],
+  imports: [FechasModule, SaldosModule],
+  controllers: [FechasController, SaldosController],
+  providers: [FechasService, SaldosService],
 })
 export class AppModule {}
